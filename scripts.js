9dcd008b0bff9ec80eee874b8c03596eaf0a46b9
Wiktoria <wpawlik73@gmail.com>
2017-05-29 00:37:35 +0200

    test5

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+//triangle area
+
+var a = prompt('Base a');
+var h = prompt('Height h');
+var triangleArea = a*h/2;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+alert('Triangle field is equal ' + triangleArea);
